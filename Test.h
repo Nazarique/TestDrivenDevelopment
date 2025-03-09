5d4149ca0fc5a10ac80fba4d52b7daf5d323cf20
Henrique Nazario <henrique.nazario.rocha@gmail.com>
2025-03-09 13:50:24 +0100

    The library has the namespace TDD. Createad a interface to store all tests by a vector pointers and a method to run them

--- Test.h
+++ Test.h
@@ -1,15 +1,31 @@
-#ifndef TEST_H
-#define TEST_H
+#ifndef TDD_TEST_H
+#define TDD_TEST_H
 
+#include <vector>
 #include <string_view>
 #include <iostream>
+
+namespace TDD
+{
+    class TestInterface {
+        public: 
+            virtual ~TestInterface() = default;
+            virtual void run() = 0;
+
+    };
+
+    std::vector<TestInterface *> & getTests() {
+        static std::vector<TestInterface *> tests;
+        return tests;
+    }
+} // namespace TDD
 
 #define TEST class Test { \
     public: \
         Test (std::string_view name) \
         : mName(name), mResult(true) \
         {} \
-        void operator () (); \
+        void operator() (); \
     private: \
         std::string mName; \
         bool mResult; \
@@ -17,4 +33,4 @@
 Test test("testCanBeCreated"); \
 void Test::operator() () 
 
-#endif // !TEST_H
+#endif // !TDD_TEST_H"
