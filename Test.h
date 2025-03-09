e7a6f35691026750ae5802ff44146cc90448e88d
Henrique Nazario <henrique.nazario.rocha@gmail.com>
2025-03-09 23:40:26 +0100

    Applied clean code concepts to modernize the current code

--- Test.h
+++ Test.h
@@ -1,11 +1,18 @@
 #ifndef TDD_TEST_H
 #define TDD_TEST_H
 
-// These macros generate unique names for tests using the line number:
-// 1. A class name, like Test7 if the TEST macro is on line 7.
-// 2. An instance name, like test7_inst.
-//
-// This ensures each test has a unique name, allowing multiple tests in the same file.
+#include <string_view>
+#include <ostream>
+#include <vector>
+#include <iostream>
+
+namespace TDD
+{
+    // These macros generate unique names for tests using the line number:
+    // 1. A class name, like Test7 if the TEST macro is on line 7.
+    // 2. An instance name, like test7_inst.
+    //
+    // This ensures each test has a unique name, allowing multiple tests in the same file.
 
 #define TDD_CLASS_FINAL(line) Test##line
 #define TDD_CLASS_RELAY(line) TDD_CLASS_FINAL(line)
@@ -15,25 +22,17 @@
 #define TDD_INSTANCE_RELAY(line) TDD_INSTANCE_FINAL(line)
 #define TDD_INSTANCE TDD_INSTANCE_RELAY(__LINE__)
 
-#include <string_view>
-#include <ostream>
-#include <vector>
-#include <iostream>
-
-namespace TDD
-{
-    static std::ostream *outStream = &std::cout;
-    // default
+    static std::ostream *outStream = &std::cout; // default
 
     class MissingException
     {
     public:
-        MissingException(std::string_view exType) : mExType(exType) {}
+        MissingException(std::string_view exceptionType) : mExceptionType(exceptionType) {}
 
-        std::string_view exType() const { return mExType; }
+        std::string_view exceptionType() const { return mExceptionType; }
 
     private:
-        std::string mExType;
+        std::string mExceptionType;
     };
 
     class TestBase
@@ -76,6 +75,133 @@ namespace TDD
         std::string mExpectedReason;
     };
 
+    class TestRunner
+    {
+    public:
+        static void runAllTests(std::vector<TestBase *> allTests)
+        {
+            TestCounters counters;
+            *outStream << "Running " << allTests.size() << " tests\n";
+
+            for (auto *test : allTests)
+            {
+                *outStream << "-------------------------\n"
+                           << test->name() << std::endl;
+                handleTest(test);
+                updateTestCounters(test, counters);
+            }
+
+            printTestSummary(counters);
+        }
+
+    private:
+        struct TestCounters
+        {
+            int passed = 0;
+            int failed = 0;
+            int missedFailures = 0;
+        };
+
+        static void printTestSummary(const TestCounters &counters)
+        {
+            *outStream << "-------------------------" << std::endl;
+            if (counters.failed == 0)
+            {
+                *outStream << "All tests passed." << std::endl;
+            }
+            else
+            {
+                *outStream << "Tests passed: " << counters.passed
+                           << "\nTests failed: " << counters.failed
+                           << "\nMissed failures: " << counters.missedFailures
+                           << std::endl;
+            }
+        }
+
+        static void handleMissingException(TestBase *test, const MissingException &ex)
+        {
+            std::string message = "Expected exception type: ";
+            message += ex.exceptionType();
+            message += " was not thrown.";
+            test->setFailed(message);
+        }
+
+        static void handleUnexpectedException(TestBase *test)
+        {
+            test->setFailed("Unexpected exception thrown.");
+        }
+
+        static void handleTest(TestBase *test)
+        {
+            try
+            {
+                test->runEx();
+            }
+            catch (const MissingException &ex)
+            {
+                handleMissingException(test, ex);
+            }
+            catch (...)
+            {
+                handleUnexpectedException(test);
+            }
+        }
+
+        static bool isExpectedFailure(const TestBase *test)
+        {
+            return !test->expectedReason().empty() && test->expectedReason() == test->reason();
+        }
+
+        static bool isMissedExpectedFailure(const TestBase *test)
+        {
+            return !test->expectedReason().empty() && test->passed();
+        }
+
+        static void verifyPassedTest(const TestBase *test, TestCounters &counters)
+        {
+            if (isMissedExpectedFailure(test))
+            {
+                ++counters.missedFailures;
+                *outStream << "Missed expected failure\n"
+                           << "Test passed but was expected to fail."
+                           << std::endl;
+            }
+            else
+            {
+                ++counters.passed;
+                *outStream << "Passed" << std::endl;
+            }
+        }
+
+        static void verifyFailedTest(const TestBase *test, TestCounters &counters)
+        {
+            if (isExpectedFailure(test))
+            {
+                ++counters.passed;
+                *outStream << "Expected failure\n"
+                           << test->reason() << std::endl;
+            }
+            else
+            {
+                ++counters.failed;
+                *outStream << "Failed\n"
+                           << test->reason() << std::endl;
+            }
+        }
+
+        static void updateTestCounters(TestBase *test, TestCounters &counters)
+        {
+            if (test->passed())
+            {
+                verifyPassedTest(test, counters);
+            }
+            else
+            {
+                verifyFailedTest(test, counters);
+            }
+        }
+    };
+
     inline void setOutStream(std::ostream &os)
     {
         outStream = &os;
@@ -87,64 +213,9 @@ namespace TDD
         return tests;
     }
 
-    inline void summaryTests(const int &testsPassed, const int &testsFailed)
-    {
-        (*outStream) << "-------------------------" << std::endl;
-        if (testsFailed == 0)
-        {
-            (*outStream) << "All tests passed." << std::endl;
-        }
-        else
-        {
-            (*outStream) << "Tests passed: " << testsPassed
-                         << "\nTests failed: " << testsFailed
-                         << std::endl;
-        }
-    }
-
     inline void runTests()
     {
-        int testsPassed = 0, testsFailed = 0;
-        (*outStream) << "Running " << getTests().size()
-                     << " tests\n";
-        for (auto *test : TDD::getTests())
-        {
-            (*outStream) << "-------------------------\n"
-                         << test->name() << std::endl;
-            try
-            {
-                test->runEx();
-            }
-            catch (MissingException const &ex)
-            {
-                std::string message = "Expected exception type: ";
-                message += ex.exType();
-                message += " was not thrown.";
-                test->setFailed(message);
-            }
-            catch (...)
-            {
-                test->setFailed("Unexpected exception thrown.");
-            }
-            if (test->passed())
-            {
-                testsPassed++;
-                (*outStream) << "Passed" << std::endl;
-            }
-            else if (not test->expectedReason().empty() && test->expectedReason() == test->reason())
-            {
-                testsPassed++;
-                (*outStream) << "Expected failure\n"
-                             << test->reason() << std::endl;
-            }
-            else
-            {
-                testsFailed++;
-                (*outStream) << "Failed\n"
-                             << test->reason() << std::endl;
-            }
-        }
-        summaryTests(testsPassed, testsFailed);
+        TestRunner::runAllTests(getTests());
     }
 } // namespace TDD
 
--- tests/main.cpp
+++ tests/main.cpp
@@ -1,10 +1,18 @@
 #include "../Test.h"
 #include <iostream>
+#include <fstream>
 
 int main(int argc, const char **argv)
 {
 
-    TDD::setOutStream(std::cout);
     TDD::runTests();
+    /*
+    std::ofstream file("output.txt");
+
+    if (file.is_open())
+    {
+        TDD::setOutStream(file);
+        TDD::runTests();
+    } */
     return 0;
 }
