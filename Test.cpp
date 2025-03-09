77d301d3fe740481f3a4ae9b73213df16a8e3b09
Henrique Nazario <henrique.nazario.rocha@gmail.com>
2025-03-09 13:37:21 +0100

    Added macros into Test.h

---
+++ Test.cpp
@@ -0,0 +1,12 @@
+#include "Test.h"
+
+TEST
+{
+    std::cout << mName << std::endl;
+}
+
+int main(int argc, const char** argv) {
+
+    test();
+    return 0;
+}
---
+++ Test.h
@@ -0,0 +1,20 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <string_view>
+#include <iostream>
+
+#define TEST class Test { \
+    public: \
+        Test (std::string_view name) \
+        : mName(name), mResult(true) \
+        {} \
+        void operator () (); \
+    private: \
+        std::string mName; \
+        bool mResult; \
+}; \
+Test test("testCanBeCreated"); \
+void Test::operator() () 
+
+#endif // !TEST_H
